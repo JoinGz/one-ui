17cfa8a5824819a2f434061425764c8b518460c7
Gz <354300537@qq.com>
2020-03-10 16:40:17 +0800

    'tooltips-增加插入到body功能'

--- components/tooltip/index.jsx
+++ components/tooltip/index.jsx
@@ -18,7 +18,7 @@ const Position = props => {
     return ReactDOM.createPortal(children, triggerRef.current);
   }
 };
-export default function Tooltip(props) {
+export default function Tooltip (props) {
   const triggerRef = useRef();
   const postionRef = useRef();
   const hoverDomRef = useRef();
@@ -57,7 +57,7 @@ export default function Tooltip(props) {
         click: mouerIn
       };
 
-  function mouerIn(e) {
+  function mouerIn (e) {
     setIsRender(true);
     setNoBlock('block');
     console.log('进入');
@@ -71,7 +71,7 @@ export default function Tooltip(props) {
       timerRef.current.isOpend = true
     }
   }
-  function mouerOut(e) {
+  function mouerOut (e) {
     if (timerRef.current.debounce) {
       clearTimeout(timerRef.current.debounce)
       // timerRef.current.debounce = null
@@ -80,19 +80,19 @@ export default function Tooltip(props) {
       console.log('出去');
       // timer定时器 -- 移出后100ms隐藏
       // timerRef.current.timer = setTimeout(() => {
-        setVisibility(false);
-        onVisibleChange && onVisibleChange(false)
-        timerRef.current.isOpend = false
-        timerRef.current.blockTimer = setTimeout(() => {
-          // blockTimer定时器 -- 动画结束后设置display:none
-          setNoBlock('none');
-        }, 200);
+      setVisibility(false);
+      onVisibleChange && onVisibleChange(false)
+      timerRef.current.isOpend = false
+      timerRef.current.blockTimer = setTimeout(() => {
+        // blockTimer定时器 -- 动画结束后设置display:none
+        setNoBlock('none');
+      }, 200);
       // }, 100)
       // debounce定时器 -- 防抖300ms
     }, 300)
   }
 
-  function clicked(e) {
+  function clicked (e) {
     if (timerRef.current.isOpend && !hoverDomRef.current.contains(e.target) && !postionRef.current.contains(e.target)) {
       console.log('关闭');
       setVisibility(false);
@@ -107,7 +107,7 @@ export default function Tooltip(props) {
   useEffect(() => {
     if (!visible) {
       Object.keys(bindTriggerEvents).forEach((item, i) => {
-      hoverDomRef.current.addEventListener(item, bindTriggerEvents[item], false);
+        hoverDomRef.current.addEventListener(item, bindTriggerEvents[item], false);
       });
       return () => {
         Object.keys(bindTriggerEvents).forEach((item, i) => {
@@ -126,51 +126,75 @@ export default function Tooltip(props) {
   // 计算hoverDom出现位置
   useEffect(() => {
     if (isRender) {
-      let positionObj = {};
-      const {
-        width,
-        height,
-        top,
-        left
-      } = triggerRef.current.getBoundingClientRect();
-      const {
-        width: pw,
-        height: ph,
-        top: pt,
-        left: pl
-      } = postionRef.current.getBoundingClientRect();
-      const { scrollX, scrollY } = window;
-      console.log('scrollX: ', scrollX);
       let path = 20;
-      positionObj = {
-        top: {
-          top: -path/2 - ph,
-          left: width / 2 - pw / 2
-        },
-        bottom: {
-          top: path + height,
-          left: width / 2 - pw / 2
-        },
-        left: {
-          top: height / 2 - ph / 2,
-          left: -pw + path * -1
-        },
-        right: {
-          top: height / 2 - ph / 2,
-          left: width + path
-        }
-      };
+      const {
+          width,
+          height,
+          top,
+          left
+        } = triggerRef.current.getBoundingClientRect();
+        const {
+          width: pw,
+          height: ph,
+          top: pt,
+          left: pl
+        } = postionRef.current.getBoundingClientRect();
+        let baseX = 0, baseY = 0 ;
+      if (appendToBody) {
+        // 插入到body的计算方式
+        const { scrollX, scrollY } = window;
+        baseX = scrollY + top;
+        baseY = scrollX + left;
+        // positionObj = {
+        //   top: {
+        //     top: scrollY + top - ph - path/2 ,
+        //     // left: scrollX + left - ((width+pw)/2-width),
+        //     left: scrollX + left + width/2 - pw/2
+        //   },
+        //   bottom: {
+        //     top: scrollY + top + ph + path/2 ,
+        //     left: scrollX + left + width/2 - pw/2
+        //   },
+        //   left: {
+        //     top: scrollY + top + height/2 - ph/2 ,
+        //     left: scrollX + left - pw - path
+        //   },
+        //   right: {
+        //     top: scrollY + top + height/2 - ph/2 ,
+        //     left: scrollX + left + width + path
+        //   }
+        // };
+      }
+      let positionObj = {
+          top: {
+            top: baseX + -path / 2 - ph,
+            left: baseY + width / 2 - pw / 2
+          },
+          bottom: {
+            top: baseX + path + height,
+            left: baseY + width / 2 - pw / 2
+          },
+          left: {
+            top: baseX + height / 2 - ph / 2,
+            left: baseY -pw - path
+          },
+          right: {
+            top: baseX + height / 2 - ph / 2,
+            left: baseY + width + path
+          }
+        };
+      
       setStyleDate({ ...positionObj[position] });
     }
   }, [isRender]);
 
-  useEffect(()=>{
+  useEffect(() => {
     // 不是默认显示的每次生成绑定事件
-    if(!visible) {
+    if (!visible) {
       if (!isHover && visibility) {
-      document.body.addEventListener('click', clicked, false)
+        document.body.addEventListener('click', clicked, false)
       }
-      return ()=>{
+      return () => {
         if (!isHover) {
           document.body.removeEventListener('click', clicked, false)
         }
@@ -209,7 +233,7 @@ Tooltip.defaultProps = {
   title: '',
   trigger: triggerTypes.hover,
   theme: themes[0],
-  onVisibleChange: (e) => {console.log('bool:' + e)},
+  onVisibleChange: (e) => { console.log('bool:' + e) },
   getPopupContainer: () => document.body,
   disabled: false,
   visible: false,
--- stories/tooltip.js
+++ stories/tooltip.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Tooltip from '../components/tooltip/index.jsx'
-import Bu from '../components/button/index.jsx'
+import Button from '../components/button/index.jsx'
 import '../components/tooltip/style.js'
 
 
@@ -10,38 +10,62 @@ storiesOf('Tooltip', module)
   .add('Tooltip2', () => (  // 这里是另一个 story
     <div style={{padding: '0 0 0 30px'}}>
       <h1>基本使用</h1>
-      <Tooltip title="789">
+      <Tooltip style={{ margin: "0 10px" }} title="基本使用 one-ui">
         <span>hover</span>
       </Tooltip>
-      <Tooltip title="789" position="bottom">
-        <Bu>hover</Bu>
-      </Tooltip>
-      <Tooltip title="789" position="left">
-        <span>hover</span>
-      </Tooltip>
-      <Tooltip title="789" position="right">
-        <span>hover</span>
-      </Tooltip>
-      <h1>Click</h1>
-      <Tooltip title="click" trigger="click">
-        <span>click</span>
-      </Tooltip>
+      <h1>位置</h1>
+        <Tooltip title="上边的朋友">
+          <Button>上</Button>
+        </Tooltip>
+        <Tooltip  title="下边的朋友" position="bottom" >
+          <Button>下</Button>
+        </Tooltip>
+        <Tooltip title="左边的朋友" position="left" >
+          <Button>左</Button>
+        </Tooltip>
+        <Tooltip title="右边的朋友" position="right">
+          <Button>右</Button>
+        </Tooltip>
+      <h1>两种触发方式</h1>
+        <Tooltip title="one-ui" trigger="hover" >
+          <Button>hover</Button>
+        </Tooltip>
+        <Tooltip title="one-ui" trigger="click">
+          <Button>click</Button>
+        </Tooltip>
       <h1>主题</h1>
-      <Tooltip title="click" trigger="click" theme="light">
-        <span>click</span>
-      </Tooltip>
-      <Tooltip title="click" trigger="click" theme="light" position="bottom">
-        <span>click</span>
-      </Tooltip>
-      <Tooltip title="click" trigger="click" theme="light" position="left">
-        <span>click</span>
-      </Tooltip>
-      <Tooltip title="click" trigger="click" theme="light" position="right">
-        <span>click</span>
-      </Tooltip>
+        <Tooltip title="click" trigger="click" theme="light">
+          <Button>click</Button>
+        </Tooltip>
+        <Tooltip title="click" trigger="click" theme="light" position="bottom" >
+          <Button>click</Button>
+        </Tooltip>
+        <Tooltip title="click" trigger="click" theme="light" position="left" >
+          <Button>click</Button>
+        </Tooltip>
+        <Tooltip title="click" trigger="click" theme="light" position="right">
+          <Button>click</Button>
+        </Tooltip>
       <h1>默认显示</h1>
-      <Tooltip title="visible"  visible>
-        <span>click</span>
-      </Tooltip>
+        <Tooltip title="visible"  visible>
+          <Button>click</Button>
+        </Tooltip>
+      <h1>回调</h1>
+        <Tooltip title="one-ui" onVisibleChange={visible => console.log("visible", visible)}>
+          <Button>click</Button>
+        </Tooltip>
+      <h1>插入body</h1>
+        <Tooltip title="body" appendToBody trigger="click">
+          <Button>click</Button>
+        </Tooltip>
+        <Tooltip title="body" appendToBody trigger="click" position="bottom" >
+          <Button>click</Button>
+        </Tooltip>
+        <Tooltip title="body" appendToBody trigger="click" position="left" >
+          <Button>click</Button>
+        </Tooltip>
+        <Tooltip title="body" appendToBody trigger="click" position="right">
+          <Button>click</Button>
+        </Tooltip>
     </div>
   ));
